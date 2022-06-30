2ed8a7e2288de96c85be03ae68308e2a0e28dbfe
John Garbero <john.garbero.itb@gmail.com>
2022-06-30 15:42:04 -0500

    commit tree cc8fd08e2b4008382b897c0b4b5d29d6e34cadfb

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 25, 'Male', 'Red', 'Kind', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 27, 'Female', 'Brown', 'Charismatic', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 30, 'Male', 'Blue', 'Sassy', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 30, 'Female', 'Purple', 'Smart', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 36, 'Male', 'Black', 'Nice', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 31, 'Male', 'Pink', 'Funny', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 36, 'Female', 'Yellow', 'Feisty', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 25, 'Male', 'Red', 'Sad', False);
